92155ca60bfbafe613c729714c414d0e2566de6a
SB <sbrandys13@gmail.com>
2017-11-06 11:42:13 +0100

    coloring total bikes count + quiz added

---
+++ quiz/questions.js
@@ -0,0 +1,129 @@
+
+var allQuestions = [
+{
+	"question": "Who played the title character in the teen sitcom musical comedy Hannah Montana?",
+	"answers": [
+	{
+		"text": "Miley Cyrus",
+		"correct": true
+	},
+	{
+		"text": "Odpowiedz 2",
+		"correct": false
+	},
+	{
+		"text": "Odpowiedz 3",
+		"correct": false
+	},
+	{
+		"text": "Odpowiedz 4",
+		"correct": false
+	}
+	]
+},
+{
+	"question": "Which US city has been hit by the most tornadoes?",
+	"answers": [
+	{
+		"text": "Odpowiedz 1",
+		"correct": false
+	},
+	{
+		"text": "Oklahoma city",
+		"correct": true
+	},
+	{
+		"text": "Odpowiedz 3",
+		"correct": false
+	},
+	{
+		"text": "Odpowiedz 4",
+		"correct": false
+	}
+	]
+},
+{
+	"question": "What is the proper term for a group of parrots?",
+	"answers": [
+	{
+		"text": "Odpowiedz 1",
+		"correct": false
+	},
+	{
+		"text": "Odpowiedz 2",
+		"correct": false
+	},
+	{
+		"text": "Pandemonium",
+		"correct": true
+	},
+	{
+		"text": "Odpowiedz 4",
+		"correct": false
+	}
+	]
+},
+{
+	"question": "Titan, Enceladus, Mimas & Iapetus are just some of the moons orbiting which planet?",
+	"answers": [
+	{
+		"text": "Odpowiedz 1",
+		"correct": false
+	},
+	{
+		"text": "Odpowiedz 2",
+		"correct": false
+	},
+	{
+		"text": "Odpowiedz 3",
+		"correct": false
+	},
+	{
+		"text": "Saturn",
+		"correct": true
+	}
+	]
+},
+{
+	"question": "Which British author wrote the popular children's novel James and the Giant Peach?",
+	"answers": [
+	{
+		"text": "Roald Dahl",
+		"correct": true
+	},
+	{
+		"text": "Odpowiedz 2",
+		"correct": false
+	},
+	{
+		"text": "Odpowiedz 3",
+		"correct": false
+	},
+	{
+		"text": "Odpowiedz 4",
+		"correct": false
+	}
+	]
+},
+{
+	"question": "In what year was the first Apple computer released?",
+	"answers": [
+	{
+		"text": "Odpowiedz 1",
+		"correct": false
+	},
+	{
+		"text": "Odpowiedz 2",
+		"correct": false
+	},
+	{
+		"text": "1976",
+		"correct": true
+	},
+	{
+		"text": "Odpowiedz 4",
+		"correct": false
+	}
+	]
+}
+];
