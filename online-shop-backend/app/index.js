8c3c094f43c638c1cc6e21e6d38b76777e034d2c
SB <sbrandys13@gmail.com>
2017-12-04 00:21:03 +0100

    Promos server

--- online-shop-backend/app/index.js
+++ online-shop-backend/app/index.js
@@ -8,8 +8,8 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // connect to MongoDB
 mongoose.connect('mongodb://admin:admin@ds123146.mlab.com:23146/online-shop-zpw')
-  .then(() =>  console.log('MongoDB: connection succesful'))
-  .catch((err) => console.error(err));
+.then(() =>  console.log('MongoDB: connection succesful'))
+.catch((err) => console.error(err));
 
 var bodyParser = require('body-parser');
 
@@ -28,15 +28,28 @@ app.use((request, response, next) => {
 })
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	next();
 });
 
 app.use('/categories', categories);
 app.use('/products', products);
 app.use('/orders', orders);
+
+var promoDuration;
+var promoStartTime;
+var promoDiscount;
+
+app.post('/promo', (request, response, next) => {
+	console.log(`${request.method} on /promo`);
+	console.log(request.body);
+	promoDuration = request.body["time"] * 60 * 1000;
+	promoDiscount = request.body["promo"];
+	promoStartTime = new Date().getTime();
+	response.status(200).end();
+});
 
 app.use((err, request, response, next) => {
   // log the error, for now just console.log
@@ -44,10 +57,38 @@ app.use((err, request, response, next) => {
   response.status(500).send('Something broke!')
 })
 
-app.listen(port, (err) => {
+var server = app.listen(port, (err) => {
 	if (err) {
 		return console.log('something bad happened', err)
 	}
 
 	console.log(`server is listening on ${port}`)
 })
+
+const io = require('socket.io').listen(server);
+
+io.on('connection' , function(client) {
+	console.log('Client connected...' );
+	client.emit('messages' , { hello: 'test' });
+});
+
+function tick () {
+	var now = new Date().getTime();
+	var timeLeft = promoDuration - (now - promoStartTime);
+
+	var status;
+	if (promoDuration === undefined || promoDiscount === undefined ||  timeLeft < 0) {
+		status = "expired";
+	} else {
+		status = "active";
+	}
+
+	var obj = {
+		"timeLeft": timeLeft,
+		"promo": promoDiscount,
+		"status": status
+	};
+	io.sockets.send(obj);
+};
+
+setInterval(tick, 1000);
